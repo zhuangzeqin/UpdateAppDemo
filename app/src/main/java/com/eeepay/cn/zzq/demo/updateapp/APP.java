55767f721fe8616caec72437d618f38b9e3a3864
zhuangzeqin <zzq@eeepay.cn>
2017-09-25 15:50:21 +0800

    app 基于公司业务升级改造流程    1 新增 ViseLog日志打印测试

--- app/src/main/java/com/eeepay/cn/zzq/demo/updateapp/APP.java
+++ app/src/main/java/com/eeepay/cn/zzq/demo/updateapp/APP.java
@@ -1,8 +1,11 @@
 package com.eeepay.cn.zzq.demo.updateapp;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.eeepay.cn.zzq.demo.updateapp.network.NetStateReceiver;
+import com.vise.log.ViseLog;
+import com.vise.log.inner.DefaultTree;
 
 /**
  * 描述：app 全局的Application
@@ -18,6 +21,14 @@ public class APP extends Application {
         instance = this;
         /*开启网络广播监听*/
         NetStateReceiver.registerNetworkStateReceiver(this);
+        ViseLog.getLogConfig()
+                .configAllowLog(true)//是否输出日志
+                .configShowBorders(true)//是否排版显示
+                .configTagPrefix("ViseLog")//设置标签前缀
+                .configFormatTag("%d{HH:mm:ss:SSS} %t %c{-5}")//个性化设置标签，默认显示包名
+                .configLevel(Log.VERBOSE);//设置日志最小输出级别，默认Log.VERBOSE
+        ViseLog.plant(new DefaultTree());//添加打印日志信息到Logcat的树
+
     }
 
     public static APP getInstance()
--- app/src/main/java/com/eeepay/cn/zzq/demo/updateapp/ui/Main2Activity.java
+++ app/src/main/java/com/eeepay/cn/zzq/demo/updateapp/ui/Main2Activity.java
@@ -1,9 +1,17 @@
 package com.eeepay.cn.zzq.demo.updateapp.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.eeepay.cn.zzq.demo.updateapp.R;
+import com.vise.log.ViseLog;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main2Activity extends Activity {
 
@@ -11,5 +19,42 @@ public class Main2Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ViseLog.d("Main2Activity", "test messag");//普通消息
+        //打印xml
+        String xml = "<xyy><test1><test2>key</test2></test1><test3>name</test3><test4>value</test4></xyy>";
+        ViseLog.xml(xml);
+        //打印json
+        String json = "{'xyy1':[{'test1':'test1'},{'test2':'test2'}],'xyy2':{'test3':'test3','test4':'test4'}}";
+        ViseLog.json(json);
+        // 打印HashMap
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            map.put("xyy" + i, "test" + i);
+        }
+        ViseLog.d(map);
+        // 打印List
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add("test" + i);
+        }
+        ViseLog.d(list);
+
+        // 打印Throwable对象
+        ViseLog.e(new NullPointerException("this object is null!"));
+        //打印Reference对象
+        ViseLog.d(new SoftReference(15));
+        //打印Intent对象
+        ViseLog.d(new Intent());
+        //打印Bundle对象
+        Bundle bundle = new Bundle();
+        bundle.putString("test","message");
+        ViseLog.d(bundle);
+        //打印基本对象
+        ViseLog.d(new Boolean(true));
+
+
+
+
+
     }
 }
